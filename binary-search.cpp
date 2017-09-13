9872a0025fd5aa8e37c217d591ef4e59a5847120
daveying <daveying@163.com>
2017-09-13 23:50:44 +0800

    add non-recursion version of binary search algorithm

--- binary-search.cpp
+++ binary-search.cpp
@@ -3,12 +3,14 @@ using namespace std;
 
 template<typename T>
 int binarySearch(T input[], int begin, int end, T target);
+template<typename T>
+int binarySearch2(T input[], int begin, int end, T target);
 
 int main(int argc, char** argv) {
     int input[] = {-68, -10, 3, 7, 8, 9, 10, 23, 23, 200, 400};
     int inputLen = sizeof(input) / sizeof(input[0]);
-    int target = 23;
-    cout << "Index of value " << target << " in input array is " << binarySearch(input, 0, inputLen, target) << endl;
+    int target = 7;
+    cout << "Index of value " << target << " in input array is " << binarySearch2(input, 0, inputLen, target) << endl;
 }
 
 template<typename T>
@@ -16,11 +18,29 @@ int binarySearch(T input[], int begin, int end, T target) {
 
     if (begin >= end) throw "invalid boundary";
 
-    int mid = (begin + end) / 2;
+    int mid = (begin + end) >> 1;
     if (input[mid] == target) return mid;
     if (begin == mid && input[mid] != target) return -1;
 
     if (input[mid] > target) return binarySearch(input, begin, mid, target);
     else return binarySearch(input, mid, end, target);
+
+}
+
+template<typename T>
+int binarySearch2(T input[], int begin, int end, T target) {
+
+    if (begin >= end) throw "invalid boundary";
+    int mid = (begin + end) >> 1;
+    while (begin != mid) {
+        if (input[mid] == target) return mid;
+        if (input[mid] > target) {
+            end = mid;
+        } else {
+            begin = mid;
+        }
+        mid = (begin + end) >> 1;
+    }
+    return -1;
 
 }
